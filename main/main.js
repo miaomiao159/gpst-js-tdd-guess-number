a32319df4d0c30126f21a504c471c242b74d7062
miaomiao159 <980249973@qq.com>
2018-05-24 22:31:17 +0800

    add numbergame

--- main/main.js
+++ main/main.js
@@ -1,6 +1,18 @@
-// Write your cade below:
-function main() {};
-
-module.exports = {
-    main
-}
+module.exports = function main(systeminput, userinput) {
+    var a = [];
+    var b = [];
+    var si=systeminput.toString();
+    var ui=userinput.toString();
+    if(si.length === ui.length) {
+        for(let i = 0; i < si.length; i++) {
+            if (si[i] == ui[i]) {
+                a.push(i);
+            }
+            else {
+                b.push(i);
+            }
+        }
+        return (a.length+'A'+b.length+'B');
+    }
+    return "";
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,21 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+    it('数字猜对而且位置也对', function() {
 
+        var result = main(1234,1234);
+        expect(result).toEqual("4A0B");
+    });
+
+    it('数字不对但位置不对', function() {
+
+        var result = main(1234,4321);
+        expect(result).toEqual("0A4B");
+    });
+
+    it('数字猜对但位置不对', function() {
+
+        var result = main(1234,4231);
+        expect(result).toEqual("2A2B");
+    });
 });
